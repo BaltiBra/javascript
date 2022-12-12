f1e3c2f04c775c95e279e4ffa77d246da4d63ef2
BaltiBra <110479346+BaltiBra@users.noreply.github.com>
2022-12-12 17:09:25 -0300

    Arrays

---
+++ js/stock_jettlag.js
@@ -0,0 +1,72 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Manga Nanatsu",
+        tipo: "mangas",
+        descripcion: "Un manga tipo shonen",
+        precio: 1000,
+        // talle: "L",
+        cantidad:0
+    },
+
+    {
+        id: 2,
+        nombre: "Manga Dragon Ball Z",
+        tipo: "mangas",
+        descripcion: "El manga mas conocido",
+        precio: 1200,
+        // talle: "L",
+        cantidad:0
+    },
+
+    {
+        id: 3,
+        nombre: "Manga Naruto",
+        tipo: "mangas",
+        descripcion: "Un manga de shonen",
+        precio: 1100,
+        // talle: "S",
+        cantidad:0
+    },
+
+    {
+        id: 4,
+        nombre: "Comic Avengers",
+        tipo: "comics",
+        descripcion: "Comic de los Avengers vol 1",
+        precio: 4000,
+        // talle: 42,
+        cantidad:0
+    },
+
+    {
+        id: 5,
+        nombre: "Comic Avengers 2",
+        tipo: "comics",
+        descripcion: "Comic de los Avengers vol 2",
+        precio: 4000,
+        // talle: 4,
+        cantidad:0
+    },
+
+    {
+        id: 6,
+        nombre: "Remera de Naruto",
+        tipo: "remera",
+        descripcion: "Una remera que re va",
+        precio: 5000,
+        talle: "S",
+        cantidad:0
+    },
+
+    {
+        id: 7,
+        nombre: "Remera de los campos",
+        tipo: "remera",
+        descripcion: "Remera especial de la seleccion",
+        precio: 5500,
+        talle: "M",
+        cantidad:0
+    }
+]
